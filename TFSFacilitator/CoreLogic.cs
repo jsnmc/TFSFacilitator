6bc99cf8f43b6d8311a1db9d2240603687665475
jsnmc <jsnmcdev@gmail.com>
2016-03-25 01:02:30 -0400

    Add code that will be the basis of the core loop -> fireoff builds, loop and check if they are completed/failed/etc.

---
+++ TFSFacilitator/CoreLogic.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TFSFacilitator
+{
+    class CoreLogic
+    {
+        // Have to wire the TFS build stop to this?
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+
+
+        protected async void OnStart()
+        {
+            try
+            {
+                Console.WriteLine("Starting service Polling");
+                await Poll();
+            }
+            catch (TaskCanceledException exception)
+            {
+                Console.WriteLine("Exception: {0}", exception);
+            }
+        }
+        // probably not needed
+        //protected void OnPause()
+        //{
+        //    Console.WriteLine("In OnPause");
+        //    serviceCoreLogic.StopUpdates = true;
+        //    while (!serviceCoreLogic.HasStopped)
+        //    {
+        //        RequestAdditionalTime(200000);
+        //        Thread.Sleep(120000);
+        //    }
+        //}
+
+        protected void OnStop()
+        {
+            Console.WriteLine("In OnStop");
+            
+        }
+
+        public void Launch()
+        {
+            // TODO: Launch all TFS builds here
+            //bool ret = Task.Run(() => );
+            
+            // Start up the polling to determine when the initiated
+            // builds are completed
+            OnStart();
+        }
+
+        private async Task Poll()
+        {
+            
+            CancellationToken cancellation = _cts.Token;
+
+            TimeSpan[] intervals =
+            {
+                TimeSpan.FromMinutes(0),
+                TimeSpan.FromMinutes(1)
+            };
+            var index = 0;
+            Console.WriteLine("Core logic loop started...");
+            while (true)
+            {
+                await Task.Delay(intervals[index], cancellation);
+                Console.WriteLine("Interval fired");
+                
+                try
+                {
+                
+
+                    //bool ret = await Task.Run(() => );
+                    //if (.HasStopped)
+                    //{
+                    //    break;
+                    //}
+
+                    if (index == 0)
+                        index = 1;
+
+                    Console.WriteLine("Interval fired");
+                }
+                catch
+                {
+                    // rerun on exception based on retry count
+                   // index = 0;
+                }
+
+                if (cancellation.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
